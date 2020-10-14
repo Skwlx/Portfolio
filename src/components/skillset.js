bfc63294f31fddb3bf9e63861745486d03dae58c
skwlx.xx@o2.pl <skwlx.xx@o2.pl>
2020-10-14 16:12:02 +0200

    Added basic animations and AOS

--- src/components/skillset.js
+++ src/components/skillset.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 import "../styles/components/skillset.scss"; 
 
 
 const SkillSet = () => {
+    useEffect(() =>{
+        AOS.init({
+            once: true,
+            offset: -10
+        })
+    })
+
     return(
         <div className="skillSet">
             <div className="skillSet__box">
-                <ul className="skillSet__box-list">
+                <ul className="skillSet__box-list" data-aos="fade-down">
                 <li>
                 <span className="skillSet__box-list__title">HTML</span>
                 <p className="skilSet__box-list__content">Good knowledge</p>
@@ -26,7 +35,7 @@ const SkillSet = () => {
                 </ul>
             </div>
             <div className="skillSet__box">
-                <ul className="skillSet__box-list">
+                <ul className="skillSet__box-list" data-aos="fade-up">
                 <li>
                 <span className="skillSet__box-list__title">JavaScript</span>
                 <p className="skilSet__box-list__content">Good knowledge, ES6 features</p>
@@ -37,11 +46,15 @@ const SkillSet = () => {
                 </li>
                 <li>
                 <span className="skillSet__box-list__title">Redux</span>
-                <p className="skilSet__box-list__content">Basic knowledge</p>
+                <p className="skilSet__box-list__content">Basic knowledge, currently learning</p>
                 </li>
                 <li>
                 <span className="skillSet__box-list__title">Node</span>
                 <p className="skilSet__box-list__content">Basic knowledge. Once used with ExpressJS</p>
+                </li>
+                <li>
+                <span className="skillSet__box-list__title">Angular</span>
+                <p className="skilSet__box-list__content">Currently learning</p>
                 </li>
                 </ul>
             </div>

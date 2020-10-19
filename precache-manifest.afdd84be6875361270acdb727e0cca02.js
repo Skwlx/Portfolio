6511704cae9d782f2407483986901ce277c6f147
skwlx.xx@o2.pl <skwlx.xx@o2.pl>
2020-10-19 14:02:55 +0200

    Updates

---
+++ precache-manifest.afdd84be6875361270acdb727e0cca02.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67250492b1276feebb05e73fdce8edf7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4231106b57d22bdecb99",
+    "url": "/static/css/2.a17fd7a1.chunk.css"
+  },
+  {
+    "revision": "0031f20fc0b23dc319b7",
+    "url": "/static/css/main.d543fe5b.chunk.css"
+  },
+  {
+    "revision": "4231106b57d22bdecb99",
+    "url": "/static/js/2.fb1bcee5.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/static/js/2.fb1bcee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0031f20fc0b23dc319b7",
+    "url": "/static/js/main.4b8c2acf.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "27b18f8cf927c3f833fbf79d40463b85",
+    "url": "/static/media/book.27b18f8c.png"
+  },
+  {
+    "revision": "cb1b190d229203e30f83be693d571d25",
+    "url": "/static/media/exchange.cb1b190d.png"
+  },
+  {
+    "revision": "088a2f6ed72c64e4e356734cdadbea75",
+    "url": "/static/media/flapit.088a2f6e.png"
+  },
+  {
+    "revision": "27577615ab929d751360ab4b8fd7da0d",
+    "url": "/static/media/gym.27577615.png"
+  },
+  {
+    "revision": "7b9cd50e1ace8b892050763138339a47",
+    "url": "/static/media/picker.7b9cd50e.png"
+  },
+  {
+    "revision": "b855ccd2e53fa8a69dc1df76dddce272",
+    "url": "/static/media/restaurant.b855ccd2.png"
+  },
+  {
+    "revision": "766b1f564e7b41509ddba49888c3b948",
+    "url": "/static/media/tictactoe.766b1f56.png"
+  }
+]);

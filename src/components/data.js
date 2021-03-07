2970a6f8c026f4ec3a27c3d4b0aebe32a04ebd14
skwlx.xx@o2.pl <skwlx.xx@o2.pl>
2021-03-07 18:50:26 +0100

    Add: New project Remove: Old projects

--- src/components/data.js
+++ src/components/data.js
@@ -2,8 +2,7 @@ import gymImage from "../images/gym.png";
 import bookImage from "../images/book.png";
 import carPickerImage from "../images/picker.png";
 import brochackiImage from "../images/brochacki.png";
-import flappitImage from "../images/flapit.png";
-import exchangeImage from "../images/exchange.png";
+import melodyParkImage from "../images/melodypark.png";
 import bindly from "../images/bindly.png";
 
 export const upSkills = {
@@ -76,15 +75,25 @@ export const projectsData = {
   projects: [
     {
       id: 13,
+      title: "MelodyPark",
+      description: "Commercial project made for RendPro. ",
+      stack: "Gatsby, Styled Components",
+      image: melodyParkImage,
+      code: "",
+      live: "https://www.melodypark.pl/",
+    },
+    {
+      id: 14,
       title: "Brochacki",
-      description: "Commercial project made for RendPro.",
+      description:
+        "Commercial project made for RendPro. Dynamic website with Strapi as a CMS.",
       stack: "Gatsby, Styled Components, Strapi",
       image: brochackiImage,
       code: "",
       live: "https://brochacki.pl/",
     },
     {
-      id: 14,
+      id: 15,
       title: "Bindly widget",
       description:
         "One of my commercial projects. I created styles for the Bindly widget in Scss and added some changes in js files.",
@@ -94,16 +103,16 @@ export const projectsData = {
       live: "https://www.bindly.pl",
     },
     {
-      id: 15,
+      id: 16,
       title: "Gym Instructor",
       description: "Template webiste made for practice.",
-      stack: "GatsbyJS, GraphQl, SCSS",
+      stack: "GatsbyJS, GraphQL, SCSS",
       image: gymImage,
       code: "https://github.com/Skwlx/GymInstructorSite",
       live: "https://gyminstructor.netlify.app",
     },
     {
-      id: 16,
+      id: 17,
       title: "BookApi",
       description: "Website made for searching books.",
       stack: "React, Google Books API, SCSS",
@@ -112,32 +121,13 @@ export const projectsData = {
       live: "https://skwlx.github.io/BookApi/",
     },
     {
-      id: 17,
+      id: 18,
       title: "Car picker",
       description: "App made for recrutation process.",
       stack: "React, Redux, SCSS",
       image: carPickerImage,
       code: "https://github.com/Skwlx/CarPicker",
       live: "https://car-picker.netlify.app",
-    },
-    {
-      id: 18,
-      title: "Flappit",
-      description:
-        "My friend made this game, so I made a website for him (Just for fun project).",
-      stack: "Html, CSS",
-      image: flappitImage,
-      code: "https://github.com/Skwlx/Flappit",
-      live: "https://snakerrek.github.io/Flappit/",
-    },
-    {
-      id: 19,
-      title: "ExchangeApp",
-      description: "Very simple API app, lets you check the currency rate.",
-      stack: "React, SCSS, Javascript",
-      image: exchangeImage,
-      code: "https://github.com/Skwlx/ExchangeApp",
-      live: "https://skwlx.github.io/ExchangeApp/",
     },
   ],
 };
--- src/components/skillset.js
+++ src/components/skillset.js
@@ -1,46 +1,45 @@
 import React, { useEffect } from "react";
 import Display from "../animations/displayAndHide";
-import AOS from 'aos';
+import AOS from "aos";
 
 import { upSkills, downSkills } from "./data";
 
-import 'aos/dist/aos.css'; 
+import "aos/dist/aos.css";
 import "../styles/components/skillset.scss";
 
-
 const SkillSet = () => {
-    useEffect(() =>{
-        AOS.init({
-            offset: -10
-        });
-        Display(".hidden-skill",["MY", "SKILLS"]);
-    })
+  useEffect(() => {
+    AOS.init({
+      offset: -10,
+    });
+    Display(".hidden-skill", ["MY", "SKILLS"]);
+  });
 
-    return(
-        <div className="skillSet">
-            <span className="hidden-text hidden-skill"></span>
-            <div className="skillSet__box">
-                <ul className="skillSet__box-list" data-aos="fade-down">
-                    {upSkills.skills.map(skill =>(
-                        <li key={skill.id}>
-                        <span className="skillSet__box-list__title">{skill.name}</span>
-                        <p className="skilSet__box-list__content">{skill.level}</p>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-            <div className="skillSet__box">
-                <ul className="skillSet__box-list" data-aos="fade-up">
-                {downSkills.skills.map(skill =>(
-                    <li>
-                    <span className="skillSet__box-list__title">{skill.name}</span>
-                    <p className="skilSet__box-list__content">{skill.level}</p>
-                    </li>
-                ))}
-                </ul>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="skillSet">
+      <span className="hidden-text hidden-skill"></span>
+      <div className="skillSet__box">
+        <ul className="skillSet__box-list" data-aos="fade-down">
+          {upSkills.skills.map((skill) => (
+            <li key={skill.id}>
+              <span className="skillSet__box-list__title">{skill.name}</span>
+              <p className="skilSet__box-list__content">{skill.level}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="skillSet__box">
+        <ul className="skillSet__box-list" data-aos="fade-up">
+          {downSkills.skills.map((skill) => (
+            <li>
+              <span className="skillSet__box-list__title">{skill.name}</span>
+              <p className="skilSet__box-list__content">{skill.level}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default SkillSet;
